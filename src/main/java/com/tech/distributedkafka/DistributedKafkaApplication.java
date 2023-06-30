ecc2c8ad93fddd939464d8247ea191e7eb3c3be0
Aakash Kumar <aakashthesid@gmail.com>
2023-06-30 23:58:46 +0530

    Initial commit

---
+++ src/main/java/com/tech/distributedkafka/DistributedKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.tech.distributedkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedKafkaApplication.class, args);
+	}
+
+}
